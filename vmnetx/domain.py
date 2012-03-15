a2b0c90c50bb8d54ef89c45f6896ddc894b73453
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2012-03-15 14:25:28 -0400

    vmnetx.domain: Replace domain UUID when generating images

--- vmnetx/domain.py
+++ vmnetx/domain.py
@@ -18,6 +18,7 @@
 from lxml import etree
 import subprocess
 from tempfile import NamedTemporaryFile
+import uuid
 
 class DomainXMLError(Exception):
     pass
@@ -67,6 +68,9 @@ class DomainXML(object):
 
         # Substitute generic VM name
         tree.xpath('/domain/name')[0].text = 'machine'
+
+        # Regenerate UUID
+        tree.xpath('/domain/uuid')[0].text = str(uuid.uuid4())
 
         # Remove path information from disk image
         disk_tag = tree.xpath('/domain/devices/disk/source')[0]
