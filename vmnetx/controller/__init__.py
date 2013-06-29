86ae340cda2ea3c88172e487fd370f5ba43d9163
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2013-06-28 20:00:02 -0400

    Add license blocks to controllers

--- vmnetx/controller/__init__.py
+++ vmnetx/controller/__init__.py
@@ -1,3 +1,20 @@
+#
+# vmnetx.controller - Interfaces for VM execution
+#
+# Copyright (C) 2008-2013 Carnegie Mellon University
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.  A copy of the GNU General Public License
+# should have been distributed along with this program in the file
+# COPYING.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+
 import gobject
 
 from ..util import ErrorBuffer, RangeConsolidator
