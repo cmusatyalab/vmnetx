cmusatyalab/vmnetx

commit 0a04f1cab29a061689c3bc784d156fe93ea46753

Update memory image's copy of domain XML at launch time
DomainXML.get_for_execution() now changes the domain UUID, and libvirt
will not resume a memory image with a different UUID than the one
provided in the XML argument.

vmnetx/controller/local/__init__.py
@@ -40,6 +40,7 @@ import uuid
 from wsgiref.handlers import format_date_time as format_rfc1123_date
 
 from ...domain import DomainXML
+from ...memory import LibvirtQemuMemoryHeader
 from ...package import Package
 from ...util import ErrorBuffer, ensure_dir, get_cache_dir
 from .. import Controller, MachineExecutionError, MachineStateError, Statistic
@@ -338,6 +339,12 @@ class LocalController(Controller):
         self._domain_xml = domain_xml.get_for_execution(self._domain_name,
                 emulator, disk_image_path, self.viewer_password,
                 use_spice=self.use_spice).xml
+
+        # Write domain XML to memory image
+        with open(self._memory_image_path, 'r+') as fh:
+            hdr = LibvirtQemuMemoryHeader(fh)
+            hdr.xml = self._domain_xml
+            hdr.write(fh)
 
         # Set configuration
         self.vm_name = package.name

commit 4768e9e39c8dd279194c7c432b551efb0f630e0c

Don't choke on VMs with no memory image
Bug introduced by 0a04f1ca.

vmnetx/controller/local/__init__.py
@@ -341,10 +341,11 @@ class LocalController(Controller):
                 use_spice=self.use_spice).xml
 
         # Write domain XML to memory image
-        with open(self._memory_image_path, 'r+') as fh:
-            hdr = LibvirtQemuMemoryHeader(fh)
-            hdr.xml = self._domain_xml
-            hdr.write(fh)
+        if self._memory_image_path is not None:
+            with open(self._memory_image_path, 'r+') as fh:
+                hdr = LibvirtQemuMemoryHeader(fh)
+                hdr.xml = self._domain_xml
+                hdr.write(fh)
 
         # Set configuration
         self.vm_name = package.name