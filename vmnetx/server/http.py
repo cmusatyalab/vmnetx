bfae60e4b5a4dbabc3839ed64c83d0bcde0a2e27
Da-Yoon Chung <dayoonc@andrew.cmu.edu>
2013-07-31 16:37:00 -0400

    vmnetx.server: Add multiple VM support
    The VMNetXServer class can now support the launch and management of
    multiple VMs.  A simple Flask HTTP server acts as a control interface
    and accepts requests to create tokens.  Local controllers are created on
    demand when users connect to the server.

---
+++ vmnetx/server/http.py
@@ -0,0 +1,76 @@
+#
+# vmnetx.server.http - HTTP control interface for server
+#
+# Copyright (C) 2013 Carnegie Mellon University
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.  A copy of the GNU General Public License
+# should have been distributed along with this program in the file
+# COPYING.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+
+from flask import Flask, Response, render_template, request
+import json
+import logging
+from urlparse import urlunsplit
+
+from ..package import Package, NeedAuthentication
+
+_log = logging.getLogger(__name__)
+
+DEFAULT_PORT = 18923
+
+
+class HttpServer(Flask):
+    def __init__(self, options, server):
+        Flask.__init__(self, __name__)
+        self._options = options
+        self._server = server
+        self.add_url_rule('/create-token', 'create-token',
+                self._create_token, methods=['POST'])
+
+    def _create_token(self):
+        try:
+            secret_key = request.headers['X-Secret-Key']
+        except KeyError:
+            return Response('Missing secret key', 400)
+        if secret_key != self._options['secret_key']:
+            return Response('Incorrect secret key', 401)
+
+        try:
+            args = json.loads(request.data)
+        except ValueError:
+            return Response('Invalid request JSON', 400)
+        try:
+            url = args['url']
+        except KeyError:
+            return Response('Invalid or missing argument', 400)
+
+        username = self._options['username']
+        password = self._options['password']
+        try:
+            package = Package(url)
+        except NeedAuthentication, e:
+            package = Package(url, scheme=e.scheme, username=username,
+                    password=password)
+        token = self._server.create_token(package)
+
+        host = self._options['host']
+        port = self._options['port']
+        hostname = host
+        if port != DEFAULT_PORT:
+            hostname += ':%d' % port
+
+        r = urlunsplit(('vmnetx', hostname, '/' + token, '', ''))
+
+        response = {
+            'url': r,
+        }
+        _log.info("Preparing VM at %s with token %s", url, token)
+        return Response(json.dumps(response), mimetype='application/json')
