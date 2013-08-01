cmusatyalab/vmnetx

commit d6e4922563ba43175168f7027b7902d0f36cb15b

Leave 8 KB of padding after domain XML in memory image
Since LocalController is now rewriting the memory image domain XML before
execution, we need to ensure that the memory image file has sufficient
room for expansion.

vmnetx/memory.py
@@ -15,6 +15,7 @@
 # for more details.
 #
 
+from __future__ import division
 import struct
 
 class MemoryImageError(Exception):
@@ -29,6 +30,9 @@ class LibvirtQemuMemoryHeader(object):
     HEADER_FORMAT = str(len(HEADER_MAGIC)) + 's19I'
     HEADER_LENGTH = struct.calcsize(HEADER_FORMAT)
     HEADER_UNUSED_VALUES = 15
+
+    XML_MINIMUM_PAD = 8 << 10
+    XML_END_ALIGNMENT = 4 << 10   # x86 page size
 
     COMPRESS_RAW = 0
     COMPRESS_XZ = 3
@@ -63,12 +67,20 @@ class LibvirtQemuMemoryHeader(object):
     def seek_body(self, fh):
         fh.seek(self.HEADER_LENGTH + self._xml_len)
 
-    def write(self, fh):
-        # Calculate header
+    def write(self, fh, extend=False):
+        # Calculate new XML length
+        if extend and (self._xml_len - 1 < len(self.xml) +
+                self.XML_MINIMUM_PAD):
+            xml_len = len(self.xml) + self.XML_MINIMUM_PAD + 1
+            # Round up the start of the memory image data to a multiple of
+            # the x86 page size
+            self._xml_len = (((self.HEADER_LENGTH + xml_len +
+                    self.XML_END_ALIGNMENT - 1) // self.XML_END_ALIGNMENT) *
+                    self.XML_END_ALIGNMENT) - self.HEADER_LENGTH
         if len(self.xml) > self._xml_len - 1:
-            # If this becomes a problem, we could write out a larger xml_len,
-            # though this must be page-aligned.
             raise MemoryImageError('self.xml is too large')
+
+        # Calculate header
         header = [self.HEADER_MAGIC,
                 self.HEADER_VERSION,
                 self._xml_len,

vmnetx/generate.py
@@ -47,7 +47,7 @@ def copy_memory(in_path, out_path, xml=None, compress=True):
         hdr.compressed = hdr.COMPRESS_XZ
     if xml is not None:
         hdr.xml = xml
-    hdr.write(fout)
+    hdr.write(fout, extend=True)
 
     # Print size of uncompressed image
     fin.seek(0, 2)

commit 7faf760d75182c2e83adf87c8eae9b7b828d332a

Fix vmnetx-generate memory image creation
LibvirtQemuMemoryHeader.seek_body() needs to seek to the original body
location, not the one defined by write(extend=True).

Bug introduced by d6e49225.

vmnetx/memory.py
@@ -68,22 +68,24 @@ class LibvirtQemuMemoryHeader(object):
         fh.seek(self.HEADER_LENGTH + self._xml_len)
 
     def write(self, fh, extend=False):
+        '''extend=True does not update the internal state used by
+        seek_body().'''
         # Calculate new XML length
-        if extend and (self._xml_len - 1 < len(self.xml) +
-                self.XML_MINIMUM_PAD):
+        xml_len = self._xml_len
+        if extend and xml_len - 1 < len(self.xml) + self.XML_MINIMUM_PAD:
             xml_len = len(self.xml) + self.XML_MINIMUM_PAD + 1
             # Round up the start of the memory image data to a multiple of
             # the x86 page size
-            self._xml_len = (((self.HEADER_LENGTH + xml_len +
+            xml_len = (((self.HEADER_LENGTH + xml_len +
                     self.XML_END_ALIGNMENT - 1) // self.XML_END_ALIGNMENT) *
                     self.XML_END_ALIGNMENT) - self.HEADER_LENGTH
-        if len(self.xml) > self._xml_len - 1:
+        if len(self.xml) > xml_len - 1:
             raise MemoryImageError('self.xml is too large')
 
         # Calculate header
         header = [self.HEADER_MAGIC,
                 self.HEADER_VERSION,
-                self._xml_len,
+                xml_len,
                 self.was_running,
                 self.compressed]
         header.extend([0] * self.HEADER_UNUSED_VALUES)
@@ -91,4 +93,4 @@ class LibvirtQemuMemoryHeader(object):
         # Write data
         fh.seek(0)
         fh.write(struct.pack(self.HEADER_FORMAT, *header))
-        fh.write(struct.pack('%ds' % self._xml_len, self.xml))
+        fh.write(struct.pack('%ds' % xml_len, self.xml))