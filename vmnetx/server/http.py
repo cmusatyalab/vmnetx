5e55fafc83ff4cfddad61bbc0525cd1200cc9fa0
Da-Yoon Chung <dayoonc@andrew.cmu.edu>
2013-08-02 15:22:30 -0400

    HttpServer: Look for secret key in headers instead of body of request
    By placing the secret key in the header, GET requests which lack a body
    can also authenticate with the HTTP server.

--- vmnetx/server/http.py
+++ vmnetx/server/http.py
@@ -37,17 +37,16 @@ class HttpServer(Flask):
 
     def _create_token(self):
         try:
-            args = json.loads(request.data)
-        except ValueError:
-            return Response('Invalid request JSON', 400)
-
-        try:
-            secret_key = args['secret_key']
+            secret_key = request.headers['X-Secret-Key']
         except KeyError:
             return Response('Missing secret key', 403)
         if secret_key != self._options['secret_key']:
             return Response('Incorrect secret key', 403)
 
+        try:
+            args = json.loads(request.data)
+        except ValueError:
+            return Response('Invalid request JSON', 400)
         try:
             url = args['url']
         except KeyError:
