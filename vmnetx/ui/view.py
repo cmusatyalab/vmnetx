cc855906b2942e1fd2dcf43dc4c399a54b8e1bfe
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2013-10-23 20:13:09 -0400

    vmnetx.ui.view: Make toolbar style on Windows consistent with Linux

--- vmnetx/ui/view.py
+++ vmnetx/ui/view.py
@@ -488,6 +488,8 @@ class VMWindow(gtk.Window):
         self.add(box)
 
         tbar = gtk.Toolbar()
+        tbar.set_style(gtk.TOOLBAR_ICONS)
+        tbar.set_icon_size(gtk.ICON_SIZE_LARGE_TOOLBAR)
         tbar.insert(self._agrp.get_action('quit').create_tool_item(), -1)
         tbar.insert(self._agrp.get_action('restart').create_tool_item(), -1)
         tbar.insert(self._agrp.get_action('screenshot').create_tool_item(), -1)
